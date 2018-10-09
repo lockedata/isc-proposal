ec35fd954e41d1b1cbf70a691212c9b71993f8a4
Maëlle Salmon <maelle.salmon@yahoo.se>
2018-10-09 12:23:14 +0200

    1st commit

--- ghgenerate.R
+++ ghgenerate.R
@@ -1,6 +1,6 @@
-proposal<-"Boilerplate ISC Proposal"
+proposal<-"Security ISC Proposal"
 proposal.file<-"isc-proposal.Rmd"
-author<-"Steph Locke"
+author<-"Locke Data"
 
 rmarkdown::render(proposal.file, output_format="html_document",
                   output_dir="out", quiet=TRUE)
